00a755e0c9b95aef351090115ca24bd1e21791ef
Denis Balunin <balunya232000@gmail.com>
2025-07-26 11:44:55 +0300

    Update h4 z1

--- homework4/z1/main.cpp
+++ homework4/z1/main.cpp
@@ -6,15 +6,16 @@ T square(T a) {
     return a * a;
 }
 
-template <>
-std::vector<int> square(std::vector<int> vec) {
+template <typename T>
+std::vector<T> square(std::vector<T> vec) {
     for(auto& elem : vec) {
         elem *= elem;
     }
     return vec;
 }
 
-void printV(const std::vector<int>& vec) {
+template <typename T>
+void printV(const std::vector<T>& vec) {
     for(const auto& i : vec) {
         std::cout << i << " ";
     }
@@ -26,9 +27,9 @@ int main() {
     std::cout << "x = " << x << std::endl;
     std::cout << "square(x) = " << square(x) << std::endl;
 
-    std::vector<int> v = {-1, 4, 8};
+    std::vector<double> v = {-1.1, 4.3, 8.2};
     printV(v);
-    std::vector<int> b = square(v);
+    std::vector<double> b = square(v);
     printV(b);
     
 
